261bd06fc2dc47be643916f57d5ac4b64c9f3f13
Aleksey Postupinsky <154533@mail.ru>
2017-03-07 14:51:58 +0300

    CriminalIntent(07.03.2017)

---
+++ src/main/java/com/example/rf123277/criminalintent/CrimeActivity.java
@@ -0,0 +1,20 @@
+package com.example.rf123277.criminalintent;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class CrimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+
+    }
+}
